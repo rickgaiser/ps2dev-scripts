0922722a66fbb76a7bee349575cf640358627c3d
Rick Gaiser <rgaiser@gmail.com>
2018-04-30 08:32:42 +0200

    Change script for new iop toolchain

--- build-gcc-iop-stage1.sh
+++ build-gcc-iop-stage1.sh
@@ -3,7 +3,7 @@
 TARG_NAME="iop"
 TARGET="iop"
 
-TARG_XTRA_OPTS=""
+TARG_XTRA_OPTS="--disable-libstdcxx"
 PROC_NR=$(nproc)
 
 ## Enter the toolchain/build directory
@@ -14,7 +14,7 @@ cd toolchain/build
 rm -rf gcc-$TARG_NAME-stage1 && mkdir gcc-$TARG_NAME-stage1 && cd gcc-$TARG_NAME-stage1 || { exit 1; }
 
 ## Configure the build.
-../../$TARG_NAME/gcc/configure --prefix="$PS2DEV/$TARG_NAME" --target="$TARGET" --enable-languages="c" --with-newlib --without-headers $TARG_XTRA_OPTS || { exit 1; }
+../../$TARG_NAME/gcc/configure --prefix="$PS2DEV/$TARG_NAME" --target="$TARGET" --enable-languages="c" --disable-nls --disable-shared --disable-libssp --disable-libmudflap --disable-threads --disable-libgomp --disable-libquadmath --disable-target-libiberty --disable-target-zlib --without-ppl --without-cloog --with-headers=no --disable-libada --disable-libatomic --disable-multilib $TARG_XTRA_OPTS || { exit 1; }
 
 ## Compile and install.
 make clean && make -j $PROC_NR && make install && make clean || { exit 1; }
